4fec2f667e309241b8898defe4d9170283ac6d38
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-04-10 20:40:39 +0200

    177.BluepringImplementableEvent.

--- BattleTankGame/Source/BattleTankGame/TankPlayerController.cpp
+++ BattleTankGame/Source/BattleTankGame/TankPlayerController.cpp
@@ -1,11 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "TankPlayerController.h"
+#include"TankAimingComponent.h"
 #include"Tank.h"
 #include"Engine/World.h"
 
 
 void ATankPlayerController::BeginPlay() {
 	Super::BeginPlay();
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent) {
+		FoundAimingComponent(AimingComponent);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Plater controller can't find aiming component at Begin Play"));
+	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime) {
