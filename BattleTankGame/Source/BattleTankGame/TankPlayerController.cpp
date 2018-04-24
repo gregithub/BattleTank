4489aeb4a21bef0a26e3647ce498b109c95402f5
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-04-25 01:07:11 +0200

    First person camera.

--- BattleTankGame/Source/BattleTankGame/TankPlayerController.cpp
+++ BattleTankGame/Source/BattleTankGame/TankPlayerController.cpp
@@ -72,7 +72,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection,FVect
 		HitResult,
 		StartLocation,
 		EndLocation,
-		ECollisionChannel::ECC_Visibility)) {
+		ECollisionChannel::ECC_Camera)) {
 		HitLocation=HitResult.Location;
 		return true;
 	}
